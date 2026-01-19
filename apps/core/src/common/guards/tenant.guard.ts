ed6bdcf1c84ff21b0632a9277ad58dbb5b5587f7
ostojics <ostojic.dev@gmail.com>
2026-01-19 19:25:24 +0100

    adjustments

--- apps/core/src/common/guards/tenant.guard.ts
+++ apps/core/src/common/guards/tenant.guard.ts
@@ -1,11 +1,15 @@
 import {CanActivate, ExecutionContext, Inject, Injectable} from '@nestjs/common';
 import {Request} from 'express';
+import {LOGGER} from 'src/libs/application/ports/di-tokens';
+import {LoggerPort} from 'src/libs/application/ports/logger.port';
 import {LicenseExpiredException, LicenseNotFoundException} from 'src/libs/domain/exceptions/license.exception';
 import {TenantNotFoundException} from 'src/libs/domain/exceptions/tenant.exception';
 import {
   ILicenseRepository,
   LICENSE_REPOSITORY,
 } from 'src/modules/licenses/domain/repositories/license.repository.interface';
+import {IAnalyticsService} from 'src/modules/shared/application/analytics/analytics.interface';
+import {ANALYTICS_SERVICE} from 'src/modules/shared/application/analytics/di-tokens';
 import {
   ITenantRepository,
   TENANT_REPOSITORY,
@@ -25,6 +29,8 @@ export class TenantGuard implements CanActivate {
   constructor(
     @Inject(TENANT_REPOSITORY) private readonly tenantRepository: ITenantRepository,
     @Inject(LICENSE_REPOSITORY) private readonly licenseRepository: ILicenseRepository,
+    @Inject(ANALYTICS_SERVICE) private readonly analyticsService: IAnalyticsService,
+    @Inject(LOGGER) private readonly logger: LoggerPort,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -32,23 +38,34 @@ export class TenantGuard implements CanActivate {
     const tenantSlug = this.extractTenantSlug(request.headers.host);
 
     if (!tenantSlug) {
+      this.logger.debug('TenantGuard: No tenant slug found in host header', request.headers);
       throw new TenantNotFoundException();
     }
 
     const tenant = await this.tenantRepository.findBySlug(tenantSlug);
-
     if (!tenant) {
-      throw new TenantNotFoundException(tenantSlug);
+      const err = new TenantNotFoundException(tenantSlug);
+      this.analyticsService.captureException(err, 'tenant_guard', {tenantSlug});
+      this.logger.debug(`TenantGuard: Tenant not found for slug ${tenantSlug}`, err);
+
+      throw err;
     }
 
     const license = await this.licenseRepository.findById(tenant.licenseId);
-
     if (!license) {
-      throw new LicenseNotFoundException(tenant.licenseId);
+      const err = new LicenseNotFoundException(tenant.licenseId);
+      this.analyticsService.captureException(err, 'tenant_guard', {tenantId: tenant.id, licenseId: tenant.licenseId});
+      this.logger.debug(`TenantGuard: License not found for id ${tenant.licenseId}`, err);
+
+      throw err;
     }
 
     if (license.isExpired) {
-      throw new LicenseExpiredException(tenant.licenseId);
+      const err = new LicenseExpiredException(tenant.licenseId);
+      this.analyticsService.captureException(err, 'tenant_guard', {tenantId: tenant.id, licenseId: tenant.licenseId});
+      this.logger.debug(`TenantGuard: License expired for id ${tenant.licenseId}`, err);
+
+      throw err;
     }
 
     request.tenant = {id: tenant.id, slug: tenant.slug};
