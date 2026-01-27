b75ef0f473533739cfdfc624c6cf0f3fffafdc2c
ostojics <ostojic.dev@gmail.com>
2026-01-27 21:35:43 +0100

    add home button and improve cache keys

--- apps/web/src/modules/apartments/hooks/use-building-info.ts
+++ apps/web/src/modules/apartments/hooks/use-building-info.ts
@@ -2,10 +2,11 @@ import {useQuery} from '@tanstack/react-query';
 
 import {getBuildingInfo} from '@/modules/api/buildings-api';
 import {queryKeys} from '@/modules/api/query-keys';
+import {getCurrentLanguage} from '@/modules/i18n/utils/get-current-language';
 
 export const useBuildingInfo = (slug: string) => {
   return useQuery({
-    queryKey: queryKeys.buildingInfo(slug),
+    queryKey: queryKeys.buildingInfo(slug, getCurrentLanguage()),
     queryFn: () => getBuildingInfo(slug),
   });
 };
