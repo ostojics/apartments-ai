7963e925f8c2f9854186b2107a564b580331399c
ostojics <ostojic.dev@gmail.com>
2026-03-14 23:12:21 +0100

    adjust migration

--- apps/core/src/migrations/1758657000600-AddTenantIdToFeedback.ts
+++ apps/core/src/migrations/1758657000600-AddTenantIdToFeedback.ts
@@ -3,9 +3,13 @@ import {MigrationInterface, QueryRunner} from 'typeorm';
 export class AddTenantIdToFeedback1758657000600 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "feedback" ADD COLUMN "tenant_id" uuid NOT NULL;`);
+    await queryRunner.query(
+      `ALTER TABLE "feedback" ADD CONSTRAINT "FK_feedback_tenant_id" FOREIGN KEY ("tenant_id") REFERENCES "tenants" ("id");`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "feedback" DROP CONSTRAINT "FK_feedback_tenant_id";`);
     await queryRunner.query(`ALTER TABLE "feedback" DROP COLUMN "tenant_id";`);
   }
 }
